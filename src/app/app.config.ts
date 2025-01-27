56bc837b6289cbb23c5a52a007acd6de7bedba7e
Joanna Wang <jojwang@google.com>
2025-01-27 22:56:10 +0000

    fix2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideAuth(() => getAuth()),
     provideFunctions(() => getFunctions()),
     provideStorage(() => getStorage()),
-    provideMessaging(() => getMessaging())
+    provideMessaging(() => getMessaging()),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlybugs-ed0f8","appId":"1:680911224639:web:7729714235215c08c59d8a","storageBucket":"friendlybugs-ed0f8.firebasestorage.app","apiKey":"AIzaSyCwbBoUjWbGyRasxOhWHyH8Q-bOXaauvOM","authDomain":"friendlybugs-ed0f8.firebaseapp.com","messagingSenderId":"680911224639","measurementId":"G-SBTW9JMNS7"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
