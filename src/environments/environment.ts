4eda614d0bbc01fc8df5dde60165d1c8df73c7e1
Joanna Wang <jojwang@google.com>
2025-01-28 17:07:39 +0000

    Fix environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCwbBoUjWbGyRasxOhWHyH8Q-bOXaauvOM",
-  authDomain: "friendlybugs-ed0f8.firebaseapp.com",
-  projectId: "friendlybugs-ed0f8",
-  storageBucket: "friendlybugs-ed0f8.firebasestorage.app",
-  messagingSenderId: "680911224639",
-  appId: "1:680911224639:web:4135b70a448eadf8c59d8a",
-  measurementId: "G-9GMTPPK9PS"
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCwbBoUjWbGyRasxOhWHyH8Q-bOXaauvOM",
+    authDomain: "friendlybugs-ed0f8.firebaseapp.com",
+    projectId: "friendlybugs-ed0f8",
+    storageBucket: "friendlybugs-ed0f8.firebasestorage.app",
+    messagingSenderId: "680911224639",
+    appId: "1:680911224639:web:4135b70a448eadf8c59d8a",
+    measurementId: "G-9GMTPPK9PS"
+  },
 };
