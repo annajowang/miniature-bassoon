b7659655224d4b29d0e23bacf4ba7121b6916e70
Joanna Wang <jojwang@google.com>
2025-01-28 16:51:25 +0000

    Adding Google Authentication.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
-export const environment = {
-  firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwbBoUjWbGyRasxOhWHyH8Q-bOXaauvOM",
+  authDomain: "friendlybugs-ed0f8.firebaseapp.com",
+  projectId: "friendlybugs-ed0f8",
+  storageBucket: "friendlybugs-ed0f8.firebasestorage.app",
+  messagingSenderId: "680911224639",
+  appId: "1:680911224639:web:4135b70a448eadf8c59d8a",
+  measurementId: "G-9GMTPPK9PS"
 };
